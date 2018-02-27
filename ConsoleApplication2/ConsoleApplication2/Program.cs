001b468a7ba6d55bd527c2c17d362d511fcb3990
Unknown <dark.maximinou@gmail.com>
2018-02-27 10:26:55 +0100

    Création projet
    Création du projet

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Library;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Hello.Greet("lol"));
+        }
+    }
+}
---
+++ Library/Library/Hello.cs
@@ -0,0 +1,13 @@
+﻿namespace Library
+{
+    public class Hello
+    {
+        
+        //renvoie des salutations
+        public static string Greet(string name)
+        {
+            string message = "hello" + name;
+            return message;
+        }
+    }
+}
